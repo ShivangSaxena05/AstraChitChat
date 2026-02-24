9099f80904d81185c4b69ae35f5efe3557e107a5
Shivang Saxena <shivangsaxena40@gmail.com>
2026-02-24 22:14:37 +0530

    Update config.ts

--- frontend/services/config.ts
+++ frontend/services/config.ts
@@ -1,8 +1,8 @@
-const IP_ADDRESS = '192.168.29.134'; // Change this to your computer's local IP
+export const API_URL = `https://astra-d3o1.onrender.com/api`;
+export const SOCKET_URL = `https://astra-d3o1.onrender.com/`;
 
-export const API_URL = `http://${IP_ADDRESS}:5000/api`;
-export const SOCKET_URL = `http://${IP_ADDRESS}:5000`;
 
 // When you deploy, you can easily swap these out:
 // export const API_URL = 'https://your-production-api.com/api';
+
 // export const SOCKET_URL = 'https://your-production-api.com';

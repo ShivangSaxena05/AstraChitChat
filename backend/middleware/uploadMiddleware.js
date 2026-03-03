24f36160a199c6c94cd616912219056599df9f54
ThAditya <aditya.vashishth95@gmail.com>
2026-03-03 22:40:18 +0530

    AWS S3 + Cloudfront connection in ENV

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,4 +1,7 @@
 const multer = require('multer');
+const multerS3 = require("multe-s3");
+const s3 = require("../config/s3");
+
 const path = require('path');
 const fs = require('fs');
 
@@ -36,7 +39,17 @@ const upload = multer({
     fileFilter: function (req, file, cb) {
         // You can add logic here to check file types (e.g., only allow images/videos)
         cb(null, true);
-    }
+    },
+
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            const fileName = Date.now() + '-' + file.originalname;
+            cb(null, fileName);
+        },
+    }),
 });
 
 module.exports = upload;
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { createPost, getFeedPosts, getShortVideos, getUserPosts } = require('../controllers/postController');
 const { protect } = require('../middleware/auth');
+const upload = require("../middleware/uploadMiddleware");
 
 // All routes in this file are prefixed with /api/posts
 
 // PROTECTED ROUTES
+
+router.post("/upload", upload.single("media"), async(req, res)=>{
+    try{
+        const cloudfrontUrl = `${process.env.CLOUDFRONT_URL}/${req.file.key}`;
+
+        res.json({
+            message: "upload successful",
+            mediaUrl: cloudfrontUrl,
+        });
+    }catch(error){
+        res.status(500).json({error : error.message});
+    } 
+});
+
+
 router.post('/upload', protect, createPost); // User must be logged in to create a post entry
 router.get('/me', protect, getUserPosts); // User must be logged in to get their own posts
 router.get('/feed', protect, getFeedPosts); // User must be logged in to view feed
